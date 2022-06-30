a6e4e7dc7911e760f82cb0aa640211f3b9d8865b
Edgar Vardanyan <e.vardan4an@gmail.com>
2022-07-01 00:14:54 +0400

    second_try

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -1,7 +1,7 @@
 package school.mjc.stage0.loops.task1;
 
 public class RepeatIsBad {
-    public void repeatIsBad(){
+    public static void repeatIsBad(){
         System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
         System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
         System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
@@ -23,5 +23,9 @@ public class RepeatIsBad {
         System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
         System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
 
+    }
+
+    public static void main(String[] args) {
+        repeatIsBad();
     }
 }
